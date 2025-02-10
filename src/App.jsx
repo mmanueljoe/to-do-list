a45b222f8c485a33746c807b0a56c2e355e24c9a
mmanueljoe <emmanuelletsu18@gmail.com>
2025-02-10 12:40:02 +0000

    advanced react concepts(useStates)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
-  const taskList = props.tasks?.map((task) => {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name){
+    const newTask = {id:"id", name, completed: false};
+    setTasks([...tasks, newTask]);
+  };
+
+  const taskList = tasks?.map((task) => {
      return (<Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />);
   });
 
@@ -12,41 +19,13 @@ function App(props) {
     <>
       <div className="todoapp stack-large">
         <h1>TodoMatic</h1>
-        <form action="" className="">
-          <h2 className="label-wrapper">
-            <label htmlFor="new-todo-input" className="label__lg">
-              What needs to be done?
-            </label>
-          </h2>
-          <input
-            type="text"
-            id="new-todo-input"
-            className="input input__lg"
-            name="text"
-            autoComplete="off"
-          />
-          <button type="submit" className="btn btn__primary btn__lg">
-            Add
-          </button>
-        </form>
+
+        <Form addTask={addTask}/>
+
         <div className="filters btn-group stack-exception">
-          <button className="btn toggle-btn" type="button" aria-pressed="true">
-            <span className="visually-hidden">Show</span>
-            <span className="">All</span>
-            <span className="visually-hidden">Tasks</span>
-          </button>
-
-          <button className="btn toggle-btn" type="button" aria-pressed="false">
-            <span className="visually-hidden">Show</span>
-            <span className="">Active</span>
-            <span className="visually-hidden">Tasks</span>
-          </button>
-
-          <button className="btn toggle-btn" type="button" aria-pressed="false">
-            <span className="visually-hidden">Show</span>
-            <span className="">Completed</span>
-            <span className="visually-hidden">Tasks</span>
-          </button>
+          <FilterButton />
+          <FilterButton />
+          <FilterButton />
         </div>
         <h2 id="list-heading">3 tasks remaining</h2>
 
